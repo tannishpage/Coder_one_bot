abce641e2fb9fbf8f2d69b54ec7727564751fbfc
ng-jason <nghyphenjason@gmail.com>
2020-12-12 20:57:00 +1000

    _get_matrix code from Tips page
    Added their code for game map

--- bot.py
+++ bot.py
@@ -11,6 +11,8 @@ BIO:
 # import any external packages by un-commenting them
 # if you'd like to test / request any additional packages - please check with the Coder One team
 import random
+import numpy as np
+import time
 from collections import deque
 # import time
 # import numpy as np
@@ -29,6 +31,14 @@ class Agent:
 	def next_move(self, game_state, player_state):
 
 		self._my_id = player_state.id # Getting my id
+
+		# test how long it takes to construct game map
+		start_time = time.time()
+		self._map = self._get_matrix(game_state)
+		end_time = time.time()
+		print(f"Time to construct game_map:  {end_time - start_time:.10f}") 
+		# about 1/10th of a tick? ~ 0.0005 seconds (depends on system)
+
 		## Agressive Bot Code ##
 		opponent_location = game_state.opponents(self._my_id)
 
@@ -38,8 +48,9 @@ class Agent:
 			# then as we wait for it to explode we must pick up bombs that are
 			# in close vacinity of the opponent.
 
-			action = self._get_next_move()
+			action = self._get_next_move(opponent_location)
 		else:
+			pass
 			# If we run out of ammo, we must,
 			# prioritize ammo collection until we have 3 bombs
 
@@ -49,7 +60,24 @@ class Agent:
 	def _find_path_to_location(self, location):
 		pass
 
-	def _get_matrix_
+	def _get_matrix(self, game_state):
+		# get matrix from here https://www.notion.so/Tips-Tricks-and-Resources-f494e36372df44368f1d8b04e41038c5
+		cols = game_state.size[0]
+		rows = game_state.size[1]
+
+		game_map = np.zeros((rows, cols)).astype(str)
+
+		for x in range(cols):
+			# print(f"x: {x}")
+			for y in range(rows):
+				entity = game_state.entity_at((x,y))
+
+				if entity is not None:
+					game_map[y][x] = entity
+				else:
+					game_map[y][x] = 9      # using 9 here as 'free' since 0 = Player 1 whaaaat
+	
+		return game_map
 
 	def _get_direction_of_location(self, location):
 
