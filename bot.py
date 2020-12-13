b17b842071971f405f9dffa6d69b9ae9ab9ecc90
tannishpage <tannishpage1@gmail.com>
2020-12-13 17:06:25 +1000

    Added BFS, but still trying to get next move

--- bot.py
+++ bot.py
@@ -17,6 +17,11 @@ from collections import deque
 # import pandas as pd
 # import sklearn
 
+class Node: # For Breadth First Search
+	def __init__(self, point, dist):
+		self.point = point
+		self.dist = dist
+
 class Agent:
 
 	def __init__(self):
@@ -25,11 +30,14 @@ class Agent:
 		'''
 		# a list of all the actions your Agent can choose from
 		self._actions = ['', 'u', 'd', 'l','r','p']
+		self._ROW = 10
+		self._COL = 12
 
 	def next_move(self, game_state, player_state):
 
 		self._my_id = player_state.id # Getting my id
 		## Agressive Bot Code ##
+		self._game_state = game_state
 		opponent_location = game_state.opponents(self._my_id)
 
 		if player_state.ammo >= 1:
@@ -37,19 +45,53 @@ class Agent:
 			# if and when we are within range of placing a bomb, we must place the bomb
 			# then as we wait for it to explode we must pick up bombs that are
 			# in close vacinity of the opponent.
+			s = [[str(e) for e in row] for row in self._find_path_to_location(
+														player_state.location,
+														opponent_location[1])]
+			lens = [max(map(len, col)) for col in zip(*s)]
+			fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
+			table = [fmt.format(*row) for row in s]
+			print ('\n'.join(table))
 
-			action = self._get_next_move()
+			print()
 		else:
 			# If we run out of ammo, we must,
 			# prioritize ammo collection until we have 3 bombs
+			pass
+		return ""
 
-		return action
+	def _isValid(self, row: int, col: int):
+		return self._game_state.is_in_bounds((row, col)) and (self._game_state.entity_at((row, col)) not in ["ib", "sb", "ob", "b"])
 
+	def _find_path_to_location(self, from_loc, to_loc):
+		visited = [[False for i in range(self._COL)] for j in range(self._ROW)]
+		distance_mat = [[-1 for i in range(self._COL)] for j in range(self._ROW)]
+		visited[from_loc[0]][from_loc[1]] = True
+		distance_mat[from_loc[0]][from_loc[1]] = 0
+		q = deque()
+		rowNum = [-1, 0, 0, 1]
+		colNum = [0, -1, 1, 0]
+		s = Node(from_loc, 0)
+		q.append(s)
 
-	def _find_path_to_location(self, location):
-		pass
+		while q:
+			curr = q.popleft()
 
-	def _get_matrix_
+			pt = curr.point
+			if pt[0] == to_loc[0] and pt[1] == to_loc[1]:
+				distance_mat[to_loc[0]][to_loc[1]] = -2
+				return distance_mat
+
+			for i in range(4):
+				row = pt[0] + rowNum[i]
+				col = pt[1] + colNum[i]
+
+				if (self._isValid(row, col) and not visited[row][col]):
+					visited[row][col] = True
+					adj_cell = Node((row, col), curr.dist + 1)
+					distance_mat[row][col] = adj_cell.dist
+					q.append(adj_cell)
+		return -1
 
 	def _get_direction_of_location(self, location):
 
