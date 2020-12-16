edcf4b2d282588632f81a94abe0fa0d6a9a2dd80
ng-jason <nghyphenjason@gmail.com>
2020-12-16 15:01:14 +1000

    Added fleeing code from fleebot.py
    Added code so that bot will flee from bomb if there is no action

--- bot.py
+++ bot.py
@@ -47,20 +47,41 @@ class Agent:
 		#start_time = time.time()
 		#self._map = self._get_matrix(game_state)
 		#end_time = time.time()
-		#print(f"Time to construct game_map:  {end_time - start_time:.10f}") 
+		#print(f"Time to construct game_map:  {end_time - start_time:.10f}")
 		# about 1/10th of a tick? ~ 0.0005 seconds (depends on system)
 
 		## Agressive Bot Code ##
 		self._game_state = game_state
+		self._location = player_state.location
 		opponent_location = game_state.opponents(self._my_id)
 		if self._bomb_down:
-			# Let's start collecting Treasure Cheasts or bombs depending on 
+			# Let's start collecting Treasure Cheasts or bombs depending on
 			# which one is more important
 			if game_state.tick_number - self._bomb_down_time >= 35:
 				self._bomb_down = False
 
 			# For now it's just collecting bombs
-			action = self._get_direction_of_location(player_state.location, self._get_closest_ammo(player_state.location, game_state.ammo))
+			action = self._get_direction_of_location(self._location, self._get_closest_ammo(player_state.location, game_state.ammo))
+
+			if action == '':  # e.g. no bomb/treasure in sight, we gotta move away from bomb
+				print("I'm on a bomb. I'm going to move.")
+				bombs_in_range = self.get_bombs_in_range(self._location, game_state.bombs)
+
+				# get our surrounding tiles
+				surrounding_tiles = self.get_surrounding_tiles(self._location)
+
+				# get list of empty tiles around us
+				empty_tiles = self.get_empty_tiles(surrounding_tiles)
+				if empty_tiles:
+
+					# get the safest tile for us to move to
+					safest_tile = self.get_safest_tile(empty_tiles, bombs_in_range)
+
+					action = self._get_direction_of_location(player_state.location, safest_tile)
+
+				else:
+					# if there isn't a free spot to move to, we're probably stuck here
+					action = ''
 
 			return action
 
@@ -138,10 +159,11 @@ class Agent:
 					q.append(adj_cell)
 		return None, None
 
-
 	def _get_direction_of_location(self, location, tile):
-
 		# see where the tile is relative to our current location
+		if tile is None:
+			return ''
+
 		diff = tuple(x-y for x, y in zip(location, tile))
 
 		# return the action that moves in the direction of the tile
@@ -163,9 +185,12 @@ class Agent:
 		minimum_point = (0, 0)
 		for ammo in ammos:
 			point, dist = self._find_path_to_location(my_loc, ammo)
+			if point is None and dist is None:
+				return None
 			if dist < minimum_dist:
 				minimum_dist = dist
 				minimum_point = point
+		print(f"Closest ammo is at {minimum_point}")
 		return minimum_point
 
 	def _get_next_move(self, from_loc, to_loc):
@@ -177,3 +202,103 @@ class Agent:
 			# Check if we are horizontally or Vertically away, if yes plant bomb
 		point, dist = self._find_path_to_location(from_loc, to_loc)
 		return self._get_direction_of_location(from_loc, point)
+
+
+
+	## CODE FROM fleebot.py ##
+
+	# returns the manhattan distance between two tiles, calculated as:
+	# 	|x1 - x2| + |y1 - y2|
+	def manhattan_distance(self, start, end):
+
+		distance = abs(start[0] - end[0]) + abs(start[1] - end[1])
+
+		return distance
+
+	# given a location as an (x,y) tuple and the bombs on the map
+	# we'll return a list of the bomb positions that are nearby
+	def get_bombs_in_range(self, location, bombs):
+
+		# empty list to store our bombs that are in range of us
+		bombs_in_range = []
+
+		# loop through all the bombs placed in the game
+		for bomb in bombs:
+
+			# get manhattan distance to a bomb
+			distance = self.manhattan_distance(location, bomb)
+
+			# set to some arbitrarily high distance
+			if distance <= 10:
+				bombs_in_range.append(bomb)
+
+		return bombs_in_range
+
+	# given a tile location as an (x,y) tuple, this function
+	# will return the surrounding tiles up, down, left and to the right as a list
+	# (i.e. [(x1,y1), (x2,y2),...])
+	# as long as they do not cross the edge of the map
+	def get_surrounding_tiles(self, location):
+		# find all the surrounding tiles relative to us
+		# location[0] = col index; location[1] = row index
+		tile_up = (location[0], location[1]+1)
+		tile_down = (location[0], location[1]-1)
+		tile_left = (location[0]-1, location[1])
+		tile_right = (location[0]+1, location[1])
+
+		# combine these into a list
+		all_surrounding_tiles = [tile_up, tile_down, tile_left, tile_right]
+
+		# we'll need to remove tiles that cross the border of the map
+		# start with an empty list to store our valid surrounding tiles
+		valid_surrounding_tiles = []
+
+		# loop through our tiles
+		for tile in all_surrounding_tiles:
+			# check if the tile is within the boundaries of the game
+			if self._game_state.is_in_bounds(tile):
+				# if yes, then add them to our list
+				valid_surrounding_tiles.append(tile)
+
+		return valid_surrounding_tiles
+
+	# given a list of tiles
+	# return the ones which are actually empty
+	def get_empty_tiles(self, tiles):
+
+		# empty list to store our empty tiles
+		empty_tiles = []
+
+		for tile in tiles:
+			if not self._game_state.is_occupied(tile):
+				# the tile isn't occupied, so we'll add it to the list
+				empty_tiles.append(tile)
+
+		return empty_tiles
+
+		# given a list of tiles and bombs
+		# find the tile that's safest to move to
+	def get_safest_tile(self, tiles, bombs):
+		# which bomb is closest to us?
+		bomb_distance = 10  # some arbitrary high distance
+		closest_bomb = bombs[0]
+
+		for bomb in bombs:
+			new_bomb_distance = self.manhattan_distance(bomb, self._location)
+			if new_bomb_distance < bomb_distance:
+				bomb_distance = new_bomb_distance
+				closest_bomb = bomb
+
+		safe_dict = {}
+		# now we'll figure out which tile is furthest away from that bomb
+		for tile in tiles:
+			# get the manhattan distance
+			distance = self.manhattan_distance(closest_bomb, tile)
+			# store this in a dictionary
+			safe_dict[tile] = distance
+
+		# return the tile with the furthest distance from any bomb
+		safest_tile = max(safe_dict, key=safe_dict.get)
+
+		return safest_tile
+
